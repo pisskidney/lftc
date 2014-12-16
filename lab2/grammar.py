f6d90ce3d13697f33a858a1e95a6137073642964
Peter Horvath <horvthpeter@gmail.com>
2014-12-16 02:07:34 +0000

    Added valid regular grammar check

--- lab2/grammar.py
+++ lab2/grammar.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 
 
+import sys
+
+
 class Grammar(object):
     arrow_symbol = '->'
+    empty_string = 'E'
 
     def __init__(self):
         self.terminals = set([])
@@ -21,8 +25,33 @@ class Grammar(object):
             new.terminals = f.readline().strip().split(' ')
             for line in f:
                 non_terminal, result = line.strip().split(self.arrow())
-                new.productions.append((non_terminal, result))
+                if len(result) == 1:
+                    assert result == self.empty_string, 'Invalid grammar!'
+                    new.productions.append((non_terminal, result[0]))
+                else:
+                    new.productions.append([
+                        non_terminal, result[0], result[1]
+                    ])
         return new
+
+    def is_regular(self):
+        return self.is_right_regular() or self.is_left_regular()
+
+    def is_right_regular(self):
+        return all([
+            prod[0] in self.non_terminals and
+            (
+                len(prod) == 2 or
+                (prod[1] in self.terminals and prod[2] in self.non_terminals)
+            )
+            for prod in self.productions
+        ])
+
+    def is_left_regular(self):
+        return all([
+            prod[1] in self.non_terminals and prod[0] in self.terminals
+            for prod in self.productions
+        ])
 
 
 class GrammarMenu(object):
@@ -32,7 +61,7 @@ class GrammarMenu(object):
     def go(self):
         print self.menu()
         choice = raw_input('Choice: ')
-        while choice != '5':
+        while choice != '6':
             if choice == '1':
                 print 'Non-terminals:'
                 print ', '.join(self.grammar.non_terminals)
@@ -42,20 +71,36 @@ class GrammarMenu(object):
             elif choice == '3':
                 print 'Productions set:'
                 for prod in self.grammar.productions:
-                    print '%s%s%s' % (prod[0], self.grammar.arrow(), prod[1])
+                    res = prod[1:]
+                    print '%s%s%s' % (
+                        prod[0], self.grammar.arrow(), ''.join(res)
+                    )
             elif choice == '4':
-                pass
+                nterm = raw_input('Please give non-terminal: ')
+                if nterm not in self.grammar.non_terminals:
+                    print 'Invalid non-terminal!'
+                else:
+                    print 'Productions for non-terminal %s:' % nterm
+                    for prod in self.grammar.productions:
+                        if prod[0] == nterm:
+                            print '%s%s%s%s' % (
+                                prod[0], self.grammar.arrow(), prod[1], prod[2]
+                            )
+            elif choice == '5':
+                print self.grammar.is_regular()
+
             print '-' * 20
             choice = raw_input('Choice: ')
 
     def menu(self):
-        return '\
-            1. Non-terminals\n\
-            2. Terminals\n\
-            3. Productions\n\
-            4. Productions for non-terminal\n\
-            5. Exit\n\
-        '
+        return ''.join([
+            '1. Non-terminals\n',
+            '2. Terminals\n',
+            '3. Productions\n',
+            '4. Productions for non-terminal\n',
+            '5. Check if grammar is regular.\n',
+            '6. Exit\n'
+        ])
 
 
 def main():
